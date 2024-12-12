8741038c5463fa5d15f452c2e2a076e93a9151b2
Yano Andriyanto <yano@xendit.co>
2024-12-12 09:25:25 +0700

    Need to click table to expand

--- popup.js
+++ popup.js
@@ -8,8 +8,13 @@ document.getElementById('toggleBtn').addEventListener('click', () => {
           target: { tabId: tabs[0].id },
           func: (hidden) => {
             const announcement = document.querySelector('.js-notice.pt-3.pb-2.px-4.px-lg-5.border-bottom.color-border-subtle.color-bg-default');
+            const expandWindow = document.querySelector('[data-testid="project-memex-info-button"]');
             if (announcement) {
               announcement.style.display = hidden ? 'none' : 'block';
+
+              if (hidden) {
+                expandWindow.click();
+              }
             }
           },
           args: [newState]
